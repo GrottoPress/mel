a892f7bd654eefdc0b5eabe606331ee37a67a9bb
akadusei <attakusiadusei@gmail.com>
2021-09-18 14:10:08 +0000

    Fix failing CI
    See https://github.com/GrottoPress/mel/actions/runs/1247968544

--- spec/carbon/deliver_later_strategy_spec.cr
+++ spec/carbon/deliver_later_strategy_spec.cr
@@ -4,6 +4,8 @@ describe Mel::Carbon::DeliverLaterStrategy do
   it "sends email" do
     email = WelcomeEmail.new("Mary", "mary@domain.tld")
 
+    Mel.settings.worker_id = 6
+
     email.deliver_later
     Mel.start_and_stop
     email.should be_delivered
