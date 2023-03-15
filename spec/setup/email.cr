5013436fc5c89c312f3e1767d335635735e1384c
akadusei <attakusiadusei@gmail.com>
2023-03-15 20:25:50 +0000

    Add missing Carbon mailer spec setup

---
+++ spec/setup/email.cr
@@ -0,0 +1,1 @@
+Spec.before_each { Carbon::DevAdapter.reset }
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,6 +4,7 @@ require "carbon"
 require "timecop"
 
 require "../src/spec"
+require "./setup/**"
 require "./support/**"
 require "../src/carbon"
 
