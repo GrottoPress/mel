a21bb27dcd23c4a6004c0c81cafc414b67d0ac56
akadusei <attakusiadusei@gmail.com>
2025-03-22 13:14:53 +0000

    Replace `loadstring()` with `string.gmatch()` in embedded Lua script
    According to GPT O3 Mini, this may improve performance.

--- src/redis.cr
+++ src/redis.cr
@@ -7,8 +7,8 @@ module Mel
     include Store
 
     private LUA = <<-'LUA'
-      local loadstring = loadstring or load
-      local unpack = table.unpack or unpack
+      local gmatch = string.gfind or string.gmatch
+      local unpack = unpack or table.unpack
 
       local function with_score(ids, score)
         local results = {}
@@ -21,13 +21,23 @@ module Mel
         return results
       end
 
+      local function split_string(input, delimiter)
+        local results = {}
+
+        for part in gmatch(input, '([^' .. delimiter .. ']+)') do
+          table.insert(results, part)
+        end
+
+        return results
+      end
+
       local function update_score(ids, score)
         if #ids ~= 0 then
           redis.call('ZADD', KEYS[1], 'XX', unpack(with_score(ids, score)))
         end
       end
 
-      local running_ids = loadstring('return ' .. ARGV[5])()
+      local running_ids = split_string(ARGV[5], ',')
       update_score(running_ids, ARGV[3])
 
       local due_ids = redis.call('ZRANGEBYSCORE', KEYS[1], ARGV[4], ARGV[1], 'LIMIT', 0, ARGV[2])
@@ -154,7 +164,7 @@ module Mel
     end
 
     private def env_to_lua
-      "{#{Task::Env.fetch.to_json.lchop('[').rchop(']')}}"
+      Task::Env.fetch.join(',')
     end
 
     private def orphan_score
