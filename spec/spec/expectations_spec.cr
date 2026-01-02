1d1eeb9811dfd8536d3d4af94e078cc71c7d9d0f
akadusei <attakusiadusei@gmail.com>
2026-01-02 22:30:49 +0000

    Add support for checking `count` in `#be_enqueued` spec expectation

--- spec/spec/expectations_spec.cr
+++ spec/spec/expectations_spec.cr
@@ -7,26 +7,63 @@ describe Mel::BeEnqueuedExpectation do
         SendEmailJob.run(address: "user@domain.tld")
 
         SendEmailJob.should be_enqueued
-        SendEmailJob.should be_enqueued(as: Mel::InstantTask)
+      end
+
+      it "passes if type matches" do
+        SendEmailJob.run_every(1.hour, address: "newuser@domain.tld")
+
+        SendEmailJob.should be_enqueued(as: Mel::PeriodicTask)
+        SendEmailJob.should be_enqueued(as: Mel::RecurringTask)
+      end
+
+      it "passes if count matches" do
+        SendEmailJob.run(address: "user@domain.tld")
+        SendEmailJob.run_every(1.hour, address: "newuser@domain.tld")
+
+        SendEmailJob.should be_enqueued(2)
+      end
+
+      it "passes if both count and type match" do
+        SendEmailJob.run(address: "user@domain.tld")
+        SendEmailJob.run_every(1.hour, address: "newuser@domain.tld")
+
+        SendEmailJob.should be_enqueued(1, as: Mel::PeriodicTask)
+        SendEmailJob.should be_enqueued(1, as: Mel::RecurringTask)
       end
 
       it "fails if job is not enqueued" do
-        expect_raises Spec::AssertionFailed, "to be enqueued" do
+        expect_raises Spec::AssertionFailed, " be enqueued" do
           SendEmailJob.should be_enqueued
-        end
-
-        expect_raises Spec::AssertionFailed, "to be enqueued as " do
-          SendEmailJob.should be_enqueued(as: Mel::InstantTask)
         end
       end
 
-      it "fails if expected type does not match" do
+      it "fails if type does not match" do
         SendEmailJob.run(address: "user@domain.tld")
 
         SendEmailJob.should be_enqueued
 
-        expect_raises Spec::AssertionFailed, "to be enqueued as " do
+        expect_raises Spec::AssertionFailed, " be enqueued as " do
           SendEmailJob.should be_enqueued(as: Mel::RecurringTask)
+        end
+      end
+
+      it "fails if count does not match" do
+        SendEmailJob.run(address: "user@domain.tld")
+
+        SendEmailJob.should be_enqueued
+
+        expect_raises Spec::AssertionFailed, " be enqueued exactly 4 times" do
+          SendEmailJob.should be_enqueued(4)
+        end
+      end
+
+      it "fails if both count and type do not match" do
+        SendEmailJob.run(address: "user@domain.tld")
+
+        SendEmailJob.should be_enqueued
+
+        expect_raises Spec::AssertionFailed, " exactly 2 times as " do
+          SendEmailJob.should be_enqueued(2, Mel::CronTask)
         end
       end
     end
@@ -34,26 +71,78 @@ describe Mel::BeEnqueuedExpectation do
     context "in negative assertions" do
       it "passes if job is not enqueued" do
         SendEmailJob.should_not be_enqueued
-        SendEmailJob.should_not be_enqueued(as: Mel::InstantTask)
+      end
+
+      it "passes if type does not match" do
+        SendEmailJob.run(address: "user@domain.tld")
+
+        SendEmailJob.should be_enqueued
+        SendEmailJob.should_not be_enqueued(as: Mel::CronTask)
+      end
+
+      it "passes if count does not match" do
+        SendEmailJob.run(address: "user@domain.tld")
+
+        SendEmailJob.should be_enqueued
+        SendEmailJob.should_not be_enqueued(4)
+      end
+
+      it "passes if both count and type do not match" do
+        SendEmailJob.run(address: "user@domain.tld")
+
+        SendEmailJob.should be_enqueued
+        SendEmailJob.should_not be_enqueued(3, as: Mel::CronTask)
       end
 
       it "fails if job is enqueued" do
         SendEmailJob.run(address: "user@domain.tld")
 
-        expect_raises Spec::AssertionFailed, "to not be enqueued" do
+        expect_raises Spec::AssertionFailed, " not be enqueued" do
           SendEmailJob.should_not be_enqueued
-        end
-
-        expect_raises Spec::AssertionFailed, "to not be enqueued as " do
-          SendEmailJob.should_not be_enqueued(as: Mel::InstantTask)
         end
       end
 
-      it "passes if expected type does not match" do
-        SendEmailJob.run(address: "user@domain.tld")
+      it "fails if type matches" do
+        SendEmailJob.run_every(1.hour, address: "newuser@domain.tld")
 
-        SendEmailJob.should be_enqueued(as: Mel::InstantTask)
-        SendEmailJob.should_not be_enqueued(as: Mel::CronTask)
+        expect_raises Spec::AssertionFailed, " not be enqueued as " do
+          SendEmailJob.should_not be_enqueued(as: Mel::PeriodicTask)
+        end
+
+        expect_raises Spec::AssertionFailed, " not be enqueued as " do
+          SendEmailJob.should_not be_enqueued(as: Mel::RecurringTask)
+        end
+      end
+
+      it "fails if count matches" do
+        SendEmailJob.run(address: "user@domain.tld")
+        SendEmailJob.run_every(1.hour, address: "newuser@domain.tld")
+
+        expect_raises Spec::AssertionFailed, " not be enqueued exactly 2 " do
+          SendEmailJob.should_not be_enqueued(2)
+        end
+      end
+
+      it "fails if both count and type match" do
+        SendEmailJob.run(address: "user@domain.tld")
+        SendEmailJob.run_every(1.hour, address: "newuser@domain.tld")
+
+        SendEmailJob.should be_enqueued(1, as: Mel::PeriodicTask)
+        SendEmailJob.should be_enqueued(1, as: Mel::RecurringTask)
+
+        expect_raises(
+          Spec::AssertionFailed,
+          " not be enqueued exactly once as "
+        ) do
+          SendEmailJob.should_not be_enqueued(1, as: Mel::PeriodicTask)
+        end
+
+        expect_raises(
+          Spec::AssertionFailed,
+          " not be enqueued exactly once as "
+        ) do
+          SendEmailJob.should_not be_enqueued(1, as: Mel::RecurringTask)
+        end
       end
     end
   end
--- src/spec/expectations.cr
+++ src/spec/expectations.cr
@@ -1,31 +1,42 @@
-def be_enqueued(as type : Mel::Task.class | Nil = nil)
-  Mel::BeEnqueuedExpectation.new(type)
+def be_enqueued(count : Int32? = nil, as type = nil)
+  Mel::BeEnqueuedExpectation.new(count, type)
+end
+
+def be_enqueued(count : Int, as type = nil)
+  Mel::BeEnqueuedExpectation.new(count.to_i, type)
 end
 
 struct Mel::BeEnqueuedExpectation
-  def initialize(@type : Mel::Task.class | Nil = nil)
+  def initialize(@count : Int32? = nil, @type : Mel::Task.class | Nil = nil)
+  end
+
+  def self.new(count : Int, type = nil)
+    new(count.to_i, type)
   end
 
   def match(job : Mel::Job::Template.class)
-    Mel::Task.find(-1).try &.any? do |task|
+    count = Mel::Task.find(-1).try &.count do |task|
       next false unless task.job.class == job
       @type.nil? || task.class <= @type.not_nil!
     end
+
+    return false if count.nil?
+    @count.nil? ? count > 0 : count == @count
   end
 
   def failure_message(job : Mel::Job::Template.class)
-    @type.try do |type|
-      return "Expected #{job} to be enqueued as #{type}"
-    end
-
-    "Expected #{job} to be enqueued"
+    "Expected #{job} to be enqueued#{failure_message_suffix}"
   end
 
   def negative_failure_message(job : Mel::Job::Template.class)
-    @type.try do |type|
-      return "Expected #{job} to not be enqueued as #{type}"
-    end
+    "Expected #{job} to not be enqueued#{failure_message_suffix}"
+  end
 
-    "Expected #{job} to not be enqueued"
+  private def failure_message_suffix
+    times_part = @count == 1 ? "once" : "#{@count} times"
+    count_part = @count.nil? ? "" : " exactly #{times_part}"
+    type_part = @type.nil? ? "" : " as #{@type}"
+
+    "#{count_part}#{type_part}"
   end
 end
